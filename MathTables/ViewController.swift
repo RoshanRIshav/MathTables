aedb5a560f3d0c5b3a353c261b86e46646e3fb67
RoshanRIshav <roshanr@v1022-rn-11-16.campus-dynamic.uwaterloo.ca>
2016-05-10 01:25:25 -0400

    a multiplication table generator for kids
    a multiplicative table generator with using a slider.

--- MathTables/ViewController.swift
+++ MathTables/ViewController.swift
@@ -8,8 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+    @IBOutlet weak var sliderVal: UISlider!
+    
+    
+    @IBAction func slidermoved(sender: AnyObject) {
+       
+        tableview.reloadData()
+    }
+    @available(iOS 2.0, *)
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    
+    
+    return 20    
+    }
+    
+    
+    @available(iOS 2.0, *)
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+    
+    
+      let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+    
+        let table = Int(sliderVal.value * 20)
+        
+        cell.textLabel?.text = String(table * (indexPath.row + 1))
+ 
+    return cell
+    }
+
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
